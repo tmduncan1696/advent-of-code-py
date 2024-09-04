2e201720a1e7ad32cfead71ee9b09e1ec6207ee0
tmduncan1696 <tmduncan1696@gmail.com>
2024-09-04 16:55:41 -0400

    Update Day 9 for Part 2

--- 2023/day9/main.py
+++ 2023/day9/main.py
@@ -11,7 +11,7 @@ def pairwise(iterable: t.Iterable) -> t.Iterable:
     return zip(a, b)
 
 def create_pattern_list(lst: list[int]):
-    x = [lst]
+    x = [deepcopy(lst)]
     for i in range(len(lst)):
         next = [b - a for a, b in pairwise(x[i])]
         x.append(next)
@@ -33,8 +33,26 @@ def get_next_value(lst: list[int]) -> int:
     next_value = get_next_value_from_pattern_list(pattern_list)
     return next_value
 
+def get_prev_value_from_pattern_list(pattern_list: list[list[int]]) -> int:
+    extrapolated_pattern_list = deepcopy(pattern_list)
+    for i in range(len(pattern_list) - 1, 0, -1):
+        a = extrapolated_pattern_list[i]
+        b = extrapolated_pattern_list[i - 1]
+        extrapolated_pattern_list[i-1].insert(0, b[0] - a[0])
+
+    out_list = extrapolated_pattern_list[0]
+    return out_list[0]
+
+def get_prev_value(lst: list[int]) -> int:
+    pattern_list = create_pattern_list(lst)
+    prev_value = get_prev_value_from_pattern_list(pattern_list)
+    return prev_value
+
 def part1(input: str) -> None:
-    print(sum([get_next_value(x) for x in input]))
+    print(f'Part 1: {sum([get_next_value(x) for x in input])}')
+
+def part2(input: str) -> None:
+    print(f'Part 2: {sum([get_prev_value(x) for x in input])}')
 
 
 def main() -> None:
@@ -52,6 +70,7 @@ def main() -> None:
         input = [[int(x.group()) for x in re.finditer(r'-?\d+', line.strip())] for line in f.readlines() if line.strip() != '']
 
     part1(input)
+    part2(input)
 
 if __name__ == '__main__':
     main()
