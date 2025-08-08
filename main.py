3e807c23ae88ce9a5fb7c706dbeae59d2ebeee06
Thomas <tmduncan@gmail.com>
2025-08-07 21:23:07 -0400

    Python file to run a particular year and day

---
+++ main.py
@@ -0,0 +1,28 @@
+import importlib
+import os
+
+
+def main(year, day, example):
+    INPUT_FOLDER = f'{args.year}/day{args.day}'
+    MODULE_FILE = os.path.join(INPUT_FOLDER, 'main')
+    MODULE_FILE = MODULE_FILE.replace('/', '.')
+    INPUT_FILE = os.path.join(
+        INPUT_FOLDER,
+        f'data/{"example_" if args.example else ""}input.txt'
+    )
+
+    module = importlib.import_module(MODULE_FILE)
+
+    module.main(INPUT_FILE)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('year', type=int)
+    parser.add_argument('day', type=int)
+    parser.add_argument('-e', '--example', action='store_true')
+
+    args = parser.parse_args()
+    main(**vars(args))
