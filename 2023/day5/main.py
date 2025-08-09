4065b5f841fb1c0523045b0d7d2331edec778ba9
Thomas <tmduncan@gmail.com>
2025-08-09 14:05:15 -0400

    2023 Day 5 Part 2

--- 2023/day5/main.py
+++ 2023/day5/main.py
@@ -4,7 +4,8 @@
 Advent of Code 2023 Day 5
 """
 
-
+import functools
+import itertools
 import re
 
 from utils import read_input_file
@@ -63,14 +64,68 @@ def main(input_file: str) -> None:
 
     print(f'Part 1: {get_min_location(seeds, maps)}')
 
+    seed_ranges = [
+        [seeds[i], seeds[i] + seeds[i + 1] - 1]
+        for i in range(0, len(seeds), 2)
+    ]
 
-if __name__ == '__main__':
-    import argparse
+    def flatten_list(matrix):
+        return list(functools.reduce(lambda x, y: x + y, matrix, []))
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-e', '--example', action='store_true')
-    args = parser.parse_args()
+    def split_range(range1, range2):
+        if max(range1) <= min(range2):
+            return [range1]
+        if min(range1) >= max(range2):
+            return [range1]
+        if min(range2) <= min(range1) < max(range1) <= max(range2):
+            return [range1]
+        if min(range1) < min(range2) < max(range2) < max(range1):
+            return [
+                [min(range1), min(range2) - 1],
+                range2,
+                [max(range2) + 1, max(range1)]
+            ]
+        if min(range1) < min(range2):
+            return [
+                [min(range1), min(range2) - 1],
+                [min(range2), max(range1)]
+            ]
+        return [
+            [min(range1), max(range2)],
+            [max(range2) + 1, max(range1)]
+        ]
 
-    INPUT_FILE = 'data/example_input.txt' if args.example else 'data/input.txt'
+    def get_new_ranges(start_ranges, mappings):
+        split_ranges = start_ranges
+        for mapping in mappings:
+            range2 = [mapping[1], mapping[1] + mapping[2] - 1]
+            split_ranges = flatten_list(
+                [split_range(r, range2) for r in split_ranges]
+            )
 
-    main(INPUT_FILE)
+        new_ranges = []
+        for mapping in mappings:
+            diff = mapping[0] - mapping[1]
+            range2 = [mapping[1], mapping[1] + mapping[2] - 1]
+            remove_split_ranges = []
+            for x in split_ranges:
+                if min(range2) <= min(x) <= max(range2):
+                    new_ranges.append([y + diff for y in x])
+                    remove_split_ranges.append(x)
+
+            split_ranges = [x for x in split_ranges if x not in remove_split_ranges]
+
+        new_ranges += split_ranges
+        return new_ranges
+
+    def get_location_ranges(seed_ranges, map_ranges):
+        r = seed_ranges
+        for m in map_ranges.values():
+            r = get_new_ranges(r, m)
+        return r
+
+    def get_min_location_from_ranges(seed_ranges, map_ranges):
+        location_ranges = get_location_ranges(seed_ranges, map_ranges)
+        return min(min(x) for x in location_ranges)
+
+    print(f'Part 2: {get_min_location_from_ranges(seed_ranges, maps)}')
