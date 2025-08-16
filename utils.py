a70034bc2ec674765ebf0df442bfa2b9fa0c4d6b
Thomas <tmduncan@gmail.com>
2025-08-15 22:38:12 -0400

    Use black to format

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ def read_input_file(input_file: str):
     """
     Read input file
     """
-    with open(input_file, encoding='utf-8') as f:
+    with open(input_file, encoding="utf-8") as f:
         input_data = f.read().strip()
 
     return input_data
