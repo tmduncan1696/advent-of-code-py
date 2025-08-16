c939ab73ab04b785bc36dae57fff3fe5dd3858f7
Thomas <tmduncan@gmail.com>
2025-08-15 22:38:20 -0400

    Use black to format

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ import os
 
 
 def main(year, day, example):
-    INPUT_FOLDER = f'{args.year}/day{args.day}'
-    MODULE_FILE = os.path.join(INPUT_FOLDER, 'main')
-    MODULE_FILE = MODULE_FILE.replace('/', '.')
+    INPUT_FOLDER = f"{args.year}/day{args.day}"
+    MODULE_FILE = os.path.join(INPUT_FOLDER, "main")
+    MODULE_FILE = MODULE_FILE.replace("/", ".")
     INPUT_FILE = os.path.join(
-        INPUT_FOLDER,
-        f'data/{"example_" if args.example else ""}input.txt'
+        INPUT_FOLDER, f'data/{"example_" if args.example else ""}input.txt'
     )
 
     module = importlib.import_module(MODULE_FILE)
@@ -20,9 +19,9 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('year', type=str)
-    parser.add_argument('day', type=str)
-    parser.add_argument('-e', '--example', action='store_true')
+    parser.add_argument("year", type=str)
+    parser.add_argument("day", type=str)
+    parser.add_argument("-e", "--example", action="store_true")
 
     args = parser.parse_args()
     main(**vars(args))
